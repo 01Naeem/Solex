92431e8fea6acf59bbae1c7df8ec8f8d8d7ed10b
Naeem Khan <khannaeem05563@gmail.com>
2026-05-11 15:24:39 +0530

    Fix apiError import path

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import ApiError from "../utils/ApiErrror.js";
+import ApiError from "../utils/ApiError.js";
 import User from "../models/User.model.js";
 import { config } from "../config/env.js";
 
